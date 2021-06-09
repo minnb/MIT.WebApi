832f0ef2201e8d6cf6a2ac3d100908f3f5131610
minhnb <minhnb.it@gmail.com>
2021-06-09 21:35:45 +0700

    first commit

---
+++ MIT.WebApi/Controllers/AirpayController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MIT.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AirpayController : ControllerBase
+    {
+        private readonly ILogger<AirpayController> _logger;
+        public AirpayController(ILogger<AirpayController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "OK";
+        }
+    }
+}
---
+++ src/MIT.EntityFrameworkCore/EntityFrameworkCore/GPAYDbContext .cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace MIT.EntityFrameworkCore
+{
+    public class GPAYDbContext : DbContext
+    {
+        public GPAYDbContext(DbContextOptions<GPAYDbContext> options)
+           : base(options)
+        {
+
+        }
+
+    }
+}
